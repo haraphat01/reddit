bd834f36b63b6100fcf54dbaccec8acda02567b4
haraphat01 <haraphat@gmail.com>
2023-04-19 12:38:13 -0700

    Added recoil state

---
+++ src/atoms/authModalAtom.ts
@@ -0,0 +1,15 @@
+import { atom } from "recoil";
+export interface AuthModalState {
+    open: boolean;
+    view: "login" | "signup" | "resetpassword"
+}
+
+const defaultModalState: AuthModalState = {
+    open: false,
+    view: "login"
+}
+
+export const AuthModalState = atom<AuthModalState>({
+    key: 'authModalState',
+    default: defaultModalState
+})
