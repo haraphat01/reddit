700f64b37f28f379d62ae44a8938eae931bbde76
haraphat01 <haraphat@gmail.com>
2023-04-19 03:09:27 -0700

    About to start the right content

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -8,13 +8,15 @@ type SearchInputProps = {
 
 const SearchInput: React.FC<SearchInputProps> = ({ }) => {
     return (
-        <Flex>
+        <Flex flexGrow={1} mr={2} align="center">
             <InputGroup>
                 <InputLeftElement
                     pointerEvents='none'
-                    children={<PhoneIcon color='gray.300' />}
+                    children={<PhoneIcon color='gray.300' mb={1}/>}
                 />
-                <Input type='tel' placeholder='Phone number' />
+                <Input placeholder='search reddit' fontSize="10pt" _placeholder={{ color: "gray.500" }} 
+                _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} 
+                _focus={{outline: "none", border: "1px solid", borderColor: "blue.500" }} height="34px" bg="gray.50"/>
             </InputGroup>
         </Flex>
     )
